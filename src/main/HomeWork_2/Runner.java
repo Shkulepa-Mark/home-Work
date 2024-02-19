31301e60a35c3f35a07fa440d2280ec3ed2f5bde
Shkulepa-Mark <skulepamark@gmail.com>
2024-02-20 00:52:26 +0300

    add selection menu into runner

--- src/main/HomeWork_2/Runner.java
+++ src/main/HomeWork_2/Runner.java
@@ -1,18 +1,43 @@
 package HomeWork_2;
 
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        Calculator calculator = new Calculator();
-        calculator.calculate();
+        System.out.println("choose task: ");
+        int select = scanner.nextInt();
 
-        NumberInterval numberInterval = new NumberInterval();
-        numberInterval.interval();
-
-        Translator translator = new Translator();
-        translator.translate();
-
-        ParityOfNumbers parityOfNumbers = new ParityOfNumbers();
-        parityOfNumbers.parity();
+        switch (select) {
+            case 1: {
+                Calculator calc = new Calculator();
+                calc.calculate();
+                break;
+            }
+            case 2:
+            {
+                NumberInterval numInterval = new NumberInterval();
+                numInterval.interval();
+                break;
+            }
+            case 3:
+            {
+                Translator translate = new Translator();
+                translate.translate();
+                break;
+            }
+            case 4:
+            {
+                ParityOfNumbers parityOfNum = new ParityOfNumbers();
+                parityOfNum.parity();
+                break;
+            }
+            default:
+            {
+                System.out.println("Wrong!");
+                break;
+            }
+        }
     }
 }
