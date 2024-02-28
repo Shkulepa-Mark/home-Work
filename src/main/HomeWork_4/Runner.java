3b488a73b7cdf0e1b0d4c86dc549ccd24c8c18c1
Shkulepa-Mark <skulepamark@gmail.com>
2024-02-28 20:24:13 +0300

    add concat words

--- src/main/HomeWork_4/Runner.java
+++ src/main/HomeWork_4/Runner.java
@@ -2,7 +2,10 @@ package HomeWork_4;
 
 public class Runner {
     public static void main(String[] args) {
-        FirstTask replaceText = new FirstTask();
-        replaceText.text();
+        /*FirstTask replaceText = new FirstTask();
+        replaceText.text();*/
+
+        SecondTask welcome = new SecondTask();
+        welcome.WelcomeTms();
     }
 }
