57e9311845b44f48e88f9007f5cb130cb260ae8a
Shkulepa-Mark <skulepamark@gmail.com>
2024-02-29 20:20:18 +0300

    add selection menu

--- src/main/HomeWork_4/Runner.java
+++ src/main/HomeWork_4/Runner.java
@@ -1,20 +1,44 @@
 package HomeWork_4;
 
+import java.util.Scanner;
+
 public class Runner {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        /*FirstTask replaceText = new FirstTask();
-        replaceText.text();*/
+        System.out.println("Choose the task (1,2,3,4,5): ");
 
-        /*SecondTask welcome = new SecondTask();
-        welcome.welcomeTms();*/
+        int select = scanner.nextInt();
 
-        /*ThirdTask replaceBefAft = new ThirdTask();
-        replaceBefAft.replaceSymbol();*/
-
-        /*FourthTask day = new FourthTask();
-        day.badDay();*/
-
-        FifthTask pass = new FifthTask();
-        pass.mainStr();
+        switch (select){
+            case 1: {
+                FirstTask replaceText = new FirstTask();
+                replaceText.text();
+                break;
+            }
+            case 2: {
+                SecondTask welcome = new SecondTask();
+                welcome.welcomeTms();
+                break;
+            }
+            case 3: {
+                ThirdTask replaceBefAft = new ThirdTask();
+                replaceBefAft.replaceSymbol();
+                break;
+            }
+            case 4: {
+                FourthTask day = new FourthTask();
+                day.badDay();
+                break;
+            }
+            case 5: {
+                FifthTask pass = new FifthTask();
+                pass.mainStr();
+                break;
+            }
+            default:
+                System.out.println("Wrong!");
+                break;
+        }
     }
 }
